4bcc428ca9e3beacaec825abf50b27df57721e80
erlichchen <chenxuewei.dev@gmail.com>
2021-05-20 10:56:40 +0800

    EWB-6 Update VUE
    Vue simple project

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,13 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Counter from './components/Counter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Counter
   }
 }
 </script>
