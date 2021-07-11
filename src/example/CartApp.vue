3c030926bf5ae579da08a31c648b705225151d25
erlichchen <chenxuewei.dev@gmail.com>
2021-07-11 22:38:03 +0800

    EWB-12  Vue Animation

--- src/example/CartApp.vue
+++ src/example/CartApp.vue
@@ -1,12 +1,31 @@
 <template>
+  <!-- 不用放到子组件中，会覆盖掉父组件中的class -->
+  <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.0/animate.min.css"
+    rel="stylesheet"
+    type="text/css"
+  />
   <div>
-    <cart-message class="success" />
+    <cart-message :show="show" class="success">
+        <template v-slot:default>
+          新增课程成功!
+        </template>
+      </cart-message>
     <!-- 新增商品 -->
     <cart-add v-model:item="item" @add-item="addItem"></cart-add>
     <!-- 列表组件 -->
     <cart-list :items="items"></cart-list>
   </div>
+
 </template>
+
+<style>
+  .success {
+    background: #4fc08d;
+    border: 1px solid #42b983;
+  }
+</style>
+
 <script>
   import CartAdd from './CartAdd.vue'
   import CartList from './CartList.vue'
@@ -30,7 +49,8 @@
     data() {
       return {
         item: '',
-        items: []
+        items: [],
+        show: false,
       }
     },
     async created() {
@@ -41,9 +61,8 @@
     methods: {
       addItem() {
         this.items.push(this.item)
+        this.show = true
       }
     }
   }
-
-
 </script>
